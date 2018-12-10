b9cfb4ed654e29d27b6fe63057ef02db735020c3
Afonso <afonso.rbn@gmail.com>
2018-12-10 11:15:20 -0200

    #

---
+++ src/main/java/br/com/uoutec/community/ediacaran/system/PluginInstaller.java
@@ -0,0 +1,5 @@
+package br.com.uoutec.community.ediacaran.system;
+
+public class PluginInstaller extends AbstractPluginInstaller{
+
+}
---
+++ src/main/java/br/com/uoutec/community/ediacaran/system/WebConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.uoutec.community.ediacaran.system;
+
+import org.brandao.brutos.annotation.ComponentScan;
+import org.brandao.brutos.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackage=WebConfiguration.class)
+public class WebConfiguration {
+
+}
