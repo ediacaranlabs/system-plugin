c7fdc1f1e062f90ac216edfdb349c14373143a41
Afonso <afonso.rbn@gmail.com>
2019-01-08 21:43:07 -0200

    #

--- src/main/java/br/com/uoutec/community/ediacaran/system/PluginInstaller.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/PluginInstaller.java
@@ -2,4 +2,10 @@ package br.com.uoutec.community.ediacaran.system;
 
 public class PluginInstaller extends AbstractPluginInstaller{
 
+	public static final String PACKAGE        = "community";
+	
+	public static final String PROVIDER       = "ediacaran";
+	
+	public static final String PLUGIN         = "user";
+	
 }
