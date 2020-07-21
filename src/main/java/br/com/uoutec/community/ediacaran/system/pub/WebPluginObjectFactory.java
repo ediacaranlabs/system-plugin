d2c63c699979cc0a6c8e25f6fa62cdb7a8ec7171
Afonso <afonso.rbn@gmail.com>
2020-07-20 22:39:57 -0300

    #

--- src/main/java/br/com/uoutec/community/ediacaran/system/pub/WebPluginObjectFactory.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/pub/WebPluginObjectFactory.java
@@ -5,26 +5,31 @@ import java.util.Properties;
 import org.brandao.brutos.ObjectFactory;
 
 import br.com.uoutec.community.ediacaran.plugins.EntityContextPlugin;
+import br.com.uoutec.community.ediacaran.plugins.EntityContextPluginProvider;
 
 public class WebPluginObjectFactory implements ObjectFactory{
 
+	private EntityContextPluginProvider entityContextPluginProvider;
+	
 	@Override
 	public Object getBean(String name) {
-		return EntityContextPlugin.getEntity(name, Object.class);
+		return entityContextPluginProvider.getEntity(name, Object.class);
 	}
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public Object getBean(Class clazz) {
-		return EntityContextPlugin.getEntity(clazz);
+		return entityContextPluginProvider.getEntity(clazz);
 	}
 
 	@Override
 	public void configure(Properties properties) {
+		entityContextPluginProvider = EntityContextPlugin.getEntity(EntityContextPluginProvider.class);
 	}
 
 	@Override
 	public void destroy() {
+		entityContextPluginProvider = null;
 	}
 
 }
