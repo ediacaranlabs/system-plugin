00a4fbb660d103196256b185c40a8789c18155f4
Brandao <Brandao@git.uoutec.com.br>
2023-08-17 21:06:10 -0300

    default

---
+++ src/main/java/br/com/uoutec/community/ediacaran/system/lock/LockUtil.java
@@ -0,0 +1,112 @@
+package br.com.uoutec.community.ediacaran.system.lock;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockUtil {
+
+	private Object[] locks;
+	
+	private ConcurrentMap<String, LockCount> count;
+	
+	public LockUtil() {
+		count = new ConcurrentHashMap<String, LockCount>();
+		locks = new Object[100];
+		
+		for(int i=0;i<locks.length;i++) {
+			locks[i] = new Object();
+		}
+	}
+	
+	public boolean lock(String name) {
+		
+		Object lock = locks[name.hashCode() % locks.length];
+		
+		LockCount c = null;
+		
+		synchronized(lock) {
+			
+			c = count.get(name);
+			
+			if(c == null) {
+				c = new LockCount(new ReentrantLock(), 1);
+				count.put(name, c);
+			}
+			else {
+				c.count++;
+			}
+			
+		}
+		
+		c.lock.lock();
+		
+		return true;
+	}
+
+	public boolean tryLock(String name, long time, TimeUnit timeUnit) throws InterruptedException {
+		
+		Object lock = locks[name.hashCode() % locks.length];
+		LockCount c = null;
+		
+		synchronized(lock) {
+			
+			c = count.get(name);
+			
+			if(c == null) {
+				c = new LockCount(new ReentrantLock(), 1);
+				count.put(name, c);
+			}
+			else {
+				c.count++;
+			}
+			
+		}
+		
+		c.lock.tryLock(time, timeUnit);
+		
+		return true;
+	}
+	
+	public void unlock(String name, boolean active) {
+		
+		Object lock = locks[name.hashCode() % locks.length];
+		LockCount c = null;
+		
+		synchronized(lock) {
+			
+			c = count.get(name);
+			
+			if(c == null) {
+				throw new IllegalStateException("lock"); 
+			}
+			
+			c.count--;
+			
+			if(c.count == 0) {
+				count.remove(name);
+			}
+		}
+		
+		if(active) {
+			c.lock.unlock();
+		}
+	}
+
+	public static class LockCount{
+		
+		public Lock lock;
+		
+		public int count;
+
+		public LockCount(Lock lock, int count) {
+			this.lock = lock;
+			this.count = count;
+		}
+		
+		
+	}
+	
+}
