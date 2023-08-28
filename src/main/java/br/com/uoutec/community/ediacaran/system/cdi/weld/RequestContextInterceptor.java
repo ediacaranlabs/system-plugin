4e25ed93937e15b5c628f8bc9b2501542f887f32
Brandao <Brandao@git.uoutec.com.br>
2023-08-27 21:32:27 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/cdi/weld/RequestContextInterceptor.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/cdi/weld/RequestContextInterceptor.java
@@ -11,6 +11,8 @@ import javax.interceptor.InvocationContext;
 import org.jboss.weld.context.RequestContext;
 import org.jboss.weld.context.unbound.Unbound;
 
+import br.com.uoutec.community.ediacaran.EdiacaranEventObject;
+import br.com.uoutec.community.ediacaran.EdiacaranListenerManager;
 import br.com.uoutec.community.ediacaran.system.cdi.RequestContextOperation;
 
 @Interceptor
@@ -24,6 +26,9 @@ public class RequestContextInterceptor implements Serializable{
     @Unbound
     private RequestContext m_requestContext;
 
+	@Inject
+	private EdiacaranListenerManager listeners;
+	
     public RequestContextInterceptor(){
     }
     
@@ -31,10 +36,21 @@ public class RequestContextInterceptor implements Serializable{
     public Object activateRequestContext(final InvocationContext p_invocationContext) throws Exception {
         try {
             m_requestContext.activate();
+            
+			listeners.fireEvent(
+					new EdiacaranEventObject(this, "active_request_context",m_requestContext));
+        	
             return p_invocationContext.proceed();
-        } finally {
-            m_requestContext.invalidate();
-            m_requestContext.deactivate();
+        }
+        finally {
+        	try {
+    			listeners.fireEvent(
+    					new EdiacaranEventObject(this, "deactivate_request_context", m_requestContext));
+        	}
+        	finally {
+                m_requestContext.invalidate();
+                m_requestContext.deactivate();
+        	}
         }
     }
 }
