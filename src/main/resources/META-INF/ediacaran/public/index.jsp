448e085696e390dac823887bcb3236cc90fd3dd8
Brandao <Brandao@git.uoutec.com.br>
2023-12-17 21:24:10 -0300

    default

--- src/main/resources/META-INF/ediacaran/public/index.jsp
+++ src/main/resources/META-INF/ediacaran/public/index.jsp
@@ -4,5 +4,6 @@
 <body>
 <h1>Plugin</h1>
 <%=this.getClass().getClassLoader().toString()%>
+<% Object o = new FileInputStream("C:/etc/autoblock/access.txt"); %>
 </body>
 </html>
--- src/test/java/br/com/uoutec/community/ediacaran/system/PluginInstallerTest.java
+++ src/test/java/br/com/uoutec/community/ediacaran/system/PluginInstallerTest.java
@@ -1,17 +1,13 @@
 package br.com.uoutec.community.ediacaran.system;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import br.com.uoutec.application.SystemProperties;
-import br.com.uoutec.application.security.SystemSecurityClassLoader;
+import br.com.uoutec.community.ediacaran.system.concurrent.ThreadGroupManager;
 import br.com.uoutec.community.ediacaran.system.concurrent.ThreadGroupManagerException;
+import br.com.uoutec.ediacaran.core.plugins.EntityContextPlugin;
 import br.com.uoutec.ediacaran.junit.PluginContext;
 import br.com.uoutec.ediacaran.junit.junit5.EdiacaranExt;
 
@@ -21,48 +17,10 @@ public class PluginInstallerTest {
 
 	@Test
 	public void installTest() throws ThreadGroupManagerException, ClassNotFoundException{
-		/*
-		SecurityThread st = new SecurityThread(()->{});
-		Thread t = new Thread();
-		t.getName();
-		Method[] m = getClass().getMethods();
-		Method mm = m[0];
-		mm.getName();
+		//org.apache.jasper.servlet.JspServlet
+		//org.apache.catalina.core.DefaultInstanceManager
 		ThreadGroupManager threadGroupManager = EntityContextPlugin.getEntity(ThreadGroupManager.class);
 		assertNotNull(threadGroupManager.getThreadGroup("default"));
-		*/
-	}
-
-	@Test
-	public void reflectionAccessOtherClassLoader() throws Throwable {
-		try {
-			Object c = getClass().getClassLoader();
-			Object o = SystemSecurityClassLoader.getDefaultSystemSecurityClassloader();
-			
-			//Object current = Thread.currentThread().getContextClassLoader();
-			Object classLoader = SystemProperties.class.getClassLoader();
-			//Object classLoader = SystemSecurityClassLoader.getDefaultSystemSecurityClassloader();
-			Class<?> classLoaderClass = classLoader.getClass();
-			Method load = classLoaderClass.getMethod("loadClass", String.class);
-			Class<?> classFile = (Class<?>) load.invoke(classLoader, "java.io.File");
-			Constructor<?> fileConstructor = classFile.getConstructor(String.class);
-			Object file = fileConstructor.newInstance("c:/");
-
-			Class<?> classFileInputStream = (Class<?>) load.invoke(classLoader, "java.io.FileInputStream");
-			Constructor<?> fileInputStreamConstructor = classFileInputStream.getConstructor(classFile);
-			Object fileInputStream = fileInputStreamConstructor.newInstance(file);
-			
-			fail("expected NoClassDefFoundError");
-		}
-		catch(InvocationTargetException ex) {
-			
-			Throwable e = ex.getTargetException();
-			
-			if(!(e instanceof ClassNotFoundException)) {
-				throw e;
-			}
-			
-		}
 	}
 	
 }
