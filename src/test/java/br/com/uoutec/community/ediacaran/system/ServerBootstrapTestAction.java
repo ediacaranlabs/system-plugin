3bc65dbb72dd4ac3a8ebd76f959f3f0dc6523512
Brandao <Brandao@git.uoutec.com.br>
2024-01-06 19:09:41 -0300

    default

---
+++ src/test/java/br/com/uoutec/community/ediacaran/system/ServerBootstrapTestAction.java
@@ -0,0 +1,14 @@
+package br.com.uoutec.community.ediacaran.system;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import br.com.uoutec.ediacaran.core.ApplicationStart;
+
+public class ServerBootstrapTestAction {
+
+	public static void main(String[] vars) throws IOException, URISyntaxException {
+		ApplicationStart.main(vars); 
+	}
+	
+}
