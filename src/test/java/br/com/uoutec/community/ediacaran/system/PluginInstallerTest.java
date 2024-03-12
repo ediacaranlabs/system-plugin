822d28780eccb1c4534e38ec2b4a7ad7e78dba68
Brandao <Brandao@git.uoutec.com.br>
2024-03-12 13:45:03 -0300

    default

--- src/test/java/br/com/uoutec/community/ediacaran/system/PluginInstallerTest.java
+++ src/test/java/br/com/uoutec/community/ediacaran/system/PluginInstallerTest.java
@@ -5,17 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import br.com.uoutec.community.ediacaran.system.cdi.ActiveRequestContext;
 import br.com.uoutec.community.ediacaran.system.concurrent.ThreadGroupManager;
 import br.com.uoutec.community.ediacaran.system.concurrent.ThreadGroupManagerException;
+import br.com.uoutec.ediacaran.core.EdiacaranBootstrap;
 import br.com.uoutec.ediacaran.core.plugins.EntityContextPlugin;
 import br.com.uoutec.ediacaran.junit.PluginContext;
 import br.com.uoutec.ediacaran.junit.junit5.EdiacaranExt;
+import br.com.uoutec.ediacaran.weld.tomcat.TomcatServerBootstrapBuilder;
 
 @ExtendWith(EdiacaranExt.class)
 @PluginContext("system")
 public class PluginInstallerTest {
 
+	public EdiacaranBootstrap getEdiacaranBootstrap() {
+		return TomcatServerBootstrapBuilder.builder()
+		.build();
+	}
+	
 	@Test
+	@ActiveRequestContext
 	public void installTest() throws ThreadGroupManagerException, ClassNotFoundException{
 		//org.apache.jasper.servlet.JspServlet
 		//org.apache.catalina.core.DefaultInstanceManager
