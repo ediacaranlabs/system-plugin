a6b6ef569c82d1736f92a928803688e6ab9e9902
Brandao <Brandao@git.uoutec.com.br>
2024-05-02 13:31:06 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/entity/EntityInheritanceLoader.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/entity/EntityInheritanceLoader.java
@@ -1,36 +1,23 @@
 package br.com.uoutec.community.ediacaran.system.entity;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import br.com.uoutec.application.scanner.DefaultScanner;
 import br.com.uoutec.application.scanner.filter.AnnotationTypeFilter;
-import br.com.uoutec.application.security.SecurityClassLoader;
-import br.com.uoutec.application.security.SecurityThread;
+import br.com.uoutec.ediacaran.core.EdiacaranScanner;
 
 public class EntityInheritanceLoader {
 
-	@SuppressWarnings("unchecked")
 	public List<Class<?>> loadEntities(String ... basePackage) {
 		
 		AnnotationTypeFilter filter = new AnnotationTypeFilter();
-		filter.setExpression(Arrays
-				.asList(EntityInheritance.class.getName()));
+		filter.setExpression(Arrays.asList(EntityInheritance.class.getName()));
 
-		DefaultScanner s = new DefaultScanner() {
-			
-			public SecurityClassLoader getClassLoader() throws IOException {
-				return (SecurityClassLoader)((SecurityThread)SecurityThread.currentThread()).getSecurityContextClassLoader();
-			}
-			
-		};
-		
-		s.setBasePackage(basePackage);
-		
-		s.addIncludeFilter(filter);
-		s.scan();
-		return s.getClassList();
+		EdiacaranScanner scanner = new EdiacaranScanner();
+		scanner.setBasePackage(basePackage);
+		scanner.addIncludeFilter(filter);
+		scanner.scan();
+		return scanner.getClassList();
 	}
 	
 }
