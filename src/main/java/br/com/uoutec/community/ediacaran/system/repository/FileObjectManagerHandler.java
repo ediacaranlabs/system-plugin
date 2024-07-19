501442d7eaadcfb157207dc1d982a44c840c101a
Brandao <Brandao@git.uoutec.com.br>
2024-07-18 21:25:06 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/repository/FileObjectManagerHandler.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/repository/FileObjectManagerHandler.java
@@ -1,13 +1,40 @@
 package br.com.uoutec.community.ediacaran.system.repository;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 import br.com.uoutec.application.io.Path;
 
 public class FileObjectManagerHandler extends AbstractFileManagerHandler{
 
+	@Override
+	public FileMetadata toMetadata(Path base, Path file) {
+		String baseName = base.normalizePath().getAbsolutePath().getFullName();
+		String pathName = file.getParent().normalizePath().getAbsolutePath().getFullName();
+		
+		if(!pathName.startsWith(baseName)) {
+			throw new IllegalStateException("path");
+		}
+		
+		pathName = pathName.substring(baseName.length());
+		pathName = pathName.replace(File.separator, "/");
+		
+		String[] parts = file.getName().split("\\.");
+		String id = parts[0];
+		String type = parts.length == 1? "obj" : parts[1];
+		
+		return new FileMetadata(pathName, id, type, new HashMap<String,Object>());
+	}
+	
+	@Override
+	public Path toFile(Path base, FileMetadata omd) {
+		String path = toFilePath(omd.getPath() + "/" + omd.getName());
+		return base.getPath(path + ".png");
+	}
+	
 	@Override
 	public Object read(Path file, FileMetadata metadata) throws IOException {
 		return file;
