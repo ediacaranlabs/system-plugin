86cbd5de57cfe83cb6ca01677e88201ddb5207be
Brandao <Brandao@git.uoutec.com.br>
2025-07-25 15:03:08 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/actions/ActionExecutor.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/actions/ActionExecutor.java
@@ -1,5 +1,6 @@
 package br.com.uoutec.community.ediacaran.system.actions;
 
+@FunctionalInterface
 public interface ActionExecutor {
 
 	void execute(ActionExecutorRequest request, ActionExecutorResponse response) throws Throwable;
