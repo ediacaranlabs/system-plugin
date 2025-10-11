5cb6a5b84da71675a6005b8689ab8ba0026b5b97
Brandao <afonso.rbn@gmail.com>
2025-10-11 16:57:48 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/concurrent/ListThreadPoolExecutor.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/concurrent/ListThreadPoolExecutor.java
@@ -4,12 +4,14 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import br.com.uoutec.application.security.SecurityThread;
+
 public class ListThreadPoolExecutor extends ThreadPoolExecutor{
 
 	public ListThreadPoolExecutor(
 			int size, int workQueueSize) {
 		super(size, size, 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>(workQueueSize));
+                new LinkedBlockingQueue<Runnable>(workQueueSize), (e)->new SecurityThread(e));
 		
 		this.setRejectedExecutionHandler(
 				new ThreadPoolExecutor.CallerRunsPolicy());
