325956af72055bd4c8d72a3976f5d6c1450cb5ca
Brandao <afonso.rbn@gmail.com>
2025-12-27 10:22:39 -0300

    default

---
+++ src/main/java/br/com/uoutec/community/ediacaran/system/lock/LockManager.java
@@ -0,0 +1,13 @@
+package br.com.uoutec.community.ediacaran.system.lock;
+
+import java.util.concurrent.TimeUnit;
+
+public interface LockManager {
+
+	void lock(String name);
+
+	void tryLock(String name, long time, TimeUnit timeUnit);
+
+	void unlock(String name, boolean active);
+	
+}
