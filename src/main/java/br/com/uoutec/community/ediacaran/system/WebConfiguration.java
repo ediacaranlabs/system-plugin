66f988e3af842cc491e779a046e19e5ca1ffb403
Brandao <afonso.rbn@gmail.com>
2026-02-18 09:52:49 -0300

    default

--- src/main/java/br/com/uoutec/community/ediacaran/system/WebConfiguration.java
+++ src/main/java/br/com/uoutec/community/ediacaran/system/WebConfiguration.java
@@ -3,8 +3,10 @@ package br.com.uoutec.community.ediacaran.system;
 import org.brandao.brutos.annotation.ComponentScan;
 import org.brandao.brutos.annotation.Configuration;
 
+import br.com.uoutec.ediacaran.web.EdiacaranWebScanner;
+
 @Configuration
-@ComponentScan(basePackage=WebConfiguration.class)
+@ComponentScan(basePackage=WebConfiguration.class, scanner = EdiacaranWebScanner.class)
 public class WebConfiguration {
 
 }
